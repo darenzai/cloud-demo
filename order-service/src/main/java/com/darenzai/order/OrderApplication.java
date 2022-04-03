dfc74451f0005911e2f0276ccf209f56e931345d
darenzai <1453566224@qq.com>
2022-04-03 23:52:33 +0800

    nacos

--- order-service/src/main/java/com/darenzai/order/OrderApplication.java
+++ order-service/src/main/java/com/darenzai/order/OrderApplication.java
@@ -1,12 +1,15 @@
 package com.darenzai.order;
 
+import com.alibaba.cloud.nacos.ribbon.NacosRule;
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-@MapperScan("com.xn2001.order.mapper")
+@MapperScan("com.darenzai.order.mapper")
 @SpringBootApplication
 public class OrderApplication {
 
@@ -18,4 +21,10 @@ public class OrderApplication {
     public RestTemplate restTemplate(){
         return new RestTemplate();
     }
+
+    @Bean
+    public IRule iRule(){
+        return new NacosRule();
+    }
+
 }
--- user-service/src/main/java/com/darenzai/user/UserApplication.java
+++ user-service/src/main/java/com/darenzai/user/UserApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.mybatis.spring.annotation.MapperScan;
 
-@MapperScan("com.xn2001.user.mapper")
+@MapperScan("com.darenzai.user.mapper")
 @SpringBootApplication
 public class UserApplication {
     public static void main(String[] args) {
